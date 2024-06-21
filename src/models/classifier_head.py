b9c1ad79a1139b77d04675c516476a1f30c75594
jelie-05 <jeremialieks@gmail.com>
2024-06-21 23:07:25 +0200

    inference added

---
+++ src/models/classifier_head.py
@@ -0,0 +1,36 @@
+import torch
+import torch.nn as nn
+from torchvision.models import resnet18
+
+class Classifier_Lidar(nn.Module):
+    def __init__(self, hp=None, in_dim=None):
+        super().__init__()
+        self.hp = hp
+
+        self.classifier_head = nn.Sequential(
+            nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1),  # output: (N, 256, 12, 39)
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.Conv2d(256, 512, kernel_size=3, stride=2, padding=1),  # output: (N, 512, 6, 20)
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.Conv2d(512, 1024, kernel_size=3, stride=2, padding=1),  # output: (N, 1024, 3, 10)
+            nn.BatchNorm2d(1024),
+            nn.ReLU(),
+            nn.AdaptiveAvgPool2d((1, 1)), # output: (N, 1024, 1, 1)
+            nn.Flatten(),
+            nn.Linear(1024, 512),
+            nn.BatchNorm1d(512),
+            nn.ReLU(),
+            nn.Linear(512, 256),
+            nn.BatchNorm1d(256),
+            nn.ReLU(),
+            nn.Linear(256, 256),
+            nn.BatchNorm1d(256),
+            nn.ReLU(),
+            nn.Linear(256, 1),
+            nn.Sigmoid()
+            )
+
+    def forward(self, x):
+        return self.classifier_head(x)
---
+++ src/models/mm_siamese.py
@@ -0,0 +1,47 @@
+import torch
+import torch.nn as nn
+from torchvision.models import resnet18
+
+
+def set_parameter_requires_grad(model, feature_extracting):
+    if feature_extracting:
+        for param in model.parameters():
+            param.requires_grad = False # Freeze
+
+
+class Image_Backbone(nn.Module):
+    def __init__(self, hp=None):
+        super().__init__()
+        self.hp = hp
+
+        model = resnet18(pretrained=True)
+        set_parameter_requires_grad(model, feature_extracting=False) # True: freeze
+
+        # Image Backbone: First 3 Block of ResNet
+        backbone_im = torch.nn.Sequential(*(list(model.children())[0:6]))
+        self.encoder_im = backbone_im
+
+    def forward(self, input):
+        output_im = self.encoder_im(input)
+        return output_im
+
+
+class Lidar_Backbone(nn.Module):
+
+    def __init__(self, hp=None):
+        super().__init__()
+        self.hp = hp
+
+        model = resnet18(pretrained=True)
+        set_parameter_requires_grad(model, feature_extracting=False) # True: freeze
+
+        # Change input channel for Lidar
+        backbone_lid = torch.nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=64, kernel_size=7, stride=2, padding=3, bias=False),
+            *(list(model.children())[1:6]))
+
+        self.encoder_lid = backbone_lid
+
+    def forward(self, input):
+        output_lid = self.encoder_lid(input)
+        return output_lid
