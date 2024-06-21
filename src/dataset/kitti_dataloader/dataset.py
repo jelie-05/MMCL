bfa3946b7d025f1a2c8013c605075fbf1b378c49
jelie-05 <jeremialieks@gmail.com>
2024-06-21 13:34:21 +0200

    updated dataloader for image and 3D lidar

---
+++ src/dataset/kitti_dataloader/dataset.py
@@ -0,0 +1,51 @@
+import os
+import torch
+import numpy as np
+from PIL import Image
+from .Dataloader import Kittiloader
+from torch.utils.data import Dataset, DataLoader
+
+
+class KittiDataset(Dataset):
+    def __init__(self,
+                 kittiDir,
+                 mode):
+        self.mode = mode
+        self.kitti_root = kittiDir
+
+        # use left image by default
+        self.kittiloader = Kittiloader(kittiDir, mode, cam=2)
+
+    def __getitem__(self, idx):
+        # load an item according to the given index
+        data_item = self.kittiloader.load_item(idx)     # get a set of data
+
+        return data_item
+
+    def __len__(self):
+        return self.kittiloader.data_length()
+
+
+class DataGenerator(object):
+    def __init__(self,
+                 KittiDir,
+                 phase,
+                 high_gpu=True):
+        self.phase = phase
+        self.high_gpu = high_gpu
+
+        if not self.phase in ['train', 'test', 'val', 'check', 'checkval']:
+            raise ValueError("Panic::Invalid phase parameter")
+        else:
+            pass
+
+        self.dataset = KittiDataset(KittiDir,
+                                    phase)
+
+    def create_data(self, batch_size, nthreads=0):
+        # use page locked gpu memory by default
+        return DataLoader(self.dataset,
+                          batch_size,
+                          shuffle=(self.phase == 'train') or (self.phase == 'check'),
+                          num_workers=nthreads,
+                          pin_memory=self.high_gpu)
