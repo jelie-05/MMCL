c45160edc68e47abee29b437b9994169fbe7e93a
jelie-05 <jeremialieks@gmail.com>
2024-08-26 09:49:15 +0200

    CL

--- src/utils/contrastive_loss.py
+++ src/utils/contrastive_loss.py
@@ -16,34 +16,28 @@ class ContrastiveLoss(nn.Module):
         distance = torch.sqrt(torch.sum((output_im - output_lid) ** 2, dim=1))
 
         if pixel_wise:
-            # distance = PixelwiseFeatureMaps(model=model_im, embeddings_value=distance.unsqueeze(1), input_image_size=(H, W))
-            # distance = distance.assign_embedding_value().squeeze(1)
+            distance = PixelwiseFeatureMaps(model=model_im, embeddings_value=distance.unsqueeze(1), input_image_size=(H, W))
+            distance = distance.assign_embedding_value().squeeze(1)
             N, H_dist, W_dist = distance.shape
             labels_broadcasted = labels.view(N, 1, 1).expand(N, H_dist, W_dist)
 
-            mask = mask.float()
-            
-            # Reshape the mask into patches of size 4x4
-            mask_reshaped = mask.unfold(2, self.patch_size, self.patch_size).unfold(3, self.patch_size, self.patch_size)
-            
-            # Check if each patch has at least one non-zero value
-            patch_has_nonzero = mask_reshaped.sum(dim=(-1, -2)) > 0
-            
-            # Expand the result back to the original size
-            patch_result = patch_has_nonzero.float().repeat_interleave(self.patch_size, dim=-1).repeat_interleave(self.patch_size, dim=-2)
-            
-            # Reshape back to the original mask size
-            mask_analyzed = patch_result.view(N, 1, H, W)
-            
-            mask_analyzed = F.interpolate(mask_analyzed, size=(H_dist, W_dist), mode='nearest').squeeze(1)
+            # mask = mask.float()
+            #
+            # # Reshape the mask into patches
+            # mask_reshaped = mask.unfold(2, self.patch_size, self.patch_size).unfold(3, self.patch_size, self.patch_size)
+            #
+            # # Check if each patch has at least one non-zero value
+            # patch_has_nonzero = mask_reshaped.sum(dim=(-1, -2)) > 0
+            #
+            # # Expand the result back to the original size
+            # patch_result = patch_has_nonzero.float().repeat_interleave(self.patch_size, dim=-1).repeat_interleave(self.patch_size, dim=-2)
+            #
+            # # Reshape back to the original mask size
+            # mask_analyzed = patch_result.view(N, 1, H, W)
+            #
+            # # mask_analyzed = F.interpolate(mask_analyzed, size=(H_dist, W_dist), mode='nearest').squeeze(1)
 
-            # torch.set_printoptions(profile='full')
-            # print(mask_analyzed[1,:,:])
-
-            # M_norm = mask_analyzed.sum(dim=(1, 2), keepdim=True)  # Shape: (N, 1, 1)
-            # epsilon = 1e-8
-            # M_norm = M_norm + epsilon
-
+            mask_analyzed = mask
             distance_final = distance * mask_analyzed
 
             positive_loss = torch.pow(distance_final, 2) * labels_broadcasted
