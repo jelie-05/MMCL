a1c744eb9aed533f5d63303178c32243b573ec4c
jelie-05 <jeremialieks@gmail.com>
2024-09-17 22:26:15 +0200

    train 240918 - small noaug

--- src/utils/contrastive_loss.py
+++ src/utils/contrastive_loss.py
@@ -5,15 +5,17 @@ import torch.nn.functional as F
 
 
 class ContrastiveLoss(nn.Module):
-    def __init__(self, margin=4.0, patch_size=16):
+    def __init__(self, margin=4.0, mode='resnet'):
         super(ContrastiveLoss, self).__init__()
         self.margin = margin
+        self.mode = mode
 
 
     def forward(self, output_im, output_lid, labels, model_im, H, W, vit, mask):
         # L2 Distances of feature embeddings
 
-        if vit:
+        if self.mode == 'vit':
+            print("Using CL for ViT")
             # Input (B, N, D). Comparing each embedding (1, D)-vectors
             distances = F.pairwise_distance(output_im, output_lid, p=2)  # Shape (B, N)
             distances_mean = distances.mean(dim=1)  # Shape (B,)
@@ -23,7 +25,8 @@ class ContrastiveLoss(nn.Module):
 
             loss_contrastive = (positive_loss + negative_loss).mean()
 
-        else:
+        elif self.mode == 'resnet':
+            print("Using pixel-wise CL for ResNet")
             # Input (B, C, H, W). Comparing each pixel (across C)
             distance = torch.sqrt(torch.sum((output_im - output_lid) ** 2, dim=1))
             N, H_dist, W_dist = distance.shape
@@ -32,5 +35,23 @@ class ContrastiveLoss(nn.Module):
             positive_loss = torch.pow(distance, 2) * labels_broadcasted
             negative_loss = torch.pow(torch.clamp(self.margin - distance, min=0.0), 2) * (1 - labels_broadcasted)
             loss_contrastive = (positive_loss + negative_loss).mean()
+
+        elif self.mode == 'resnet_instance':
+            N = output_im.shape[0]
+
+            # Flatten the embeddings
+            output_im_flat = output_im.view(N, -1)  # Shape: [N, D]
+            output_lid_flat = output_lid.view(N, -1)  # Shape: [N, D]
+
+            # Compute the L2 distance between flattened embeddings
+            instance_wise_distance = torch.sqrt(torch.sum((output_im_flat - output_lid_flat) ** 2, dim=1))  # Shape: [N]
+
+            labels = labels.view(N)  # Shape: [N]
+
+            positive_loss = torch.pow(instance_wise_distance, 2) * labels
+            negative_loss = torch.pow(torch.clamp(self.margin - instance_wise_distance, min=0.0), 2) * (1 - labels)
+            loss_contrastive = (positive_loss + negative_loss).mean()
+        else:
+            raise ValueError(f"Mode '{self.mode}' is not supported. Choose from 'vit', 'resnet', or 'resnet_instance'.")
 
         return loss_contrastive
