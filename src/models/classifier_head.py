33ae2e81ba71acdb029ff9164dc628d4f9f703a5
jelie-05 <jeremialieks@gmail.com>
2024-09-22 07:06:30 +0200

    retrain cls 0922

--- src/models/classifier_head.py
+++ src/models/classifier_head.py
@@ -58,9 +58,23 @@ class classifier_head(nn.Module):
                 # nn.Conv2d(input_channel, first_channel, kernel_size=3, stride=2, padding=1),  # output: (N,
                 # nn.BatchNorm2d(first_channel),
                 # nn.ReLU(),
+                # nn.AdaptiveAvgPool2d((1, 1)),
+                # nn.Flatten(),
+                # nn.Linear(first_channel, 512),
+                # nn.BatchNorm1d(512),
+                # nn.ReLU(),
+                # nn.Linear(512, 256),
+                # nn.BatchNorm1d(256),
+                # nn.ReLU(),
+                # nn.Linear(256, 256),
+                # nn.BatchNorm1d(256),
+                # nn.ReLU(),
+                # nn.Linear(256, 1),
+                # nn.Sigmoid()
+                # )
                 nn.AdaptiveAvgPool2d((1, 1)),
                 nn.Flatten(),
-                nn.Linear(first_channel, 512),
+                nn.Linear(input_channel, 512),
                 nn.BatchNorm1d(512),
                 nn.ReLU(),
                 nn.Linear(512, 256),
@@ -68,7 +82,7 @@ class classifier_head(nn.Module):
                 nn.ReLU(),
                 nn.Linear(256, 1),
                 nn.Sigmoid()
-                )
+            )
         else:
             raise ValueError("Error: starting_epoch couldn't be found")
     def load_classifier_layers(self, state_dict):
