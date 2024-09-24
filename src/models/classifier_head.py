c1888116f46fd0d706ca1acd3e6ee75a98966974
jelie-05 <jeremialieks@gmail.com>
2024-09-24 13:13:43 +0200

    retrain

--- src/models/classifier_head.py
+++ src/models/classifier_head.py
@@ -29,42 +29,42 @@ class classifier_head(nn.Module):
         first_channel = input_channel*2
 
         if model_name == 'resnet18_small':
-            self.classifier_layers = nn.Sequential(
-                nn.Conv2d(input_channel, first_channel, kernel_size=3, stride=2, padding=1),  # output: (N, 512, 12, 39)
-                nn.BatchNorm2d(first_channel),
-                nn.ReLU(),
-                nn.Conv2d(first_channel, 1024, kernel_size=3, stride=2, padding=1),  # output: (N, 512, 6, 20)
-                nn.BatchNorm2d(1024),
-                nn.ReLU(),
-                nn.Conv2d(1024, 2048, kernel_size=3, stride=2, padding=1),  # output: (N, 1024, 3, 10)
-                nn.BatchNorm2d(2048),
-                nn.ReLU(),
-                nn.AdaptiveAvgPool2d((1, 1)), # output: (N, 1024, 1, 1)
-                nn.Flatten(),
-                nn.Linear(2048, 512),
-                nn.BatchNorm1d(512),
-                nn.ReLU(),
-                nn.Linear(512, 256),
-                nn.BatchNorm1d(256),
-                nn.ReLU(),
-                nn.Linear(256, 256),
-                nn.BatchNorm1d(256),
-                nn.ReLU(),
-                nn.Linear(256, 1),
-                nn.Sigmoid()
-                )
             # self.classifier_layers = nn.Sequential(
-            #     nn.Conv2d(input_channel, first_channel, kernel_size=3, stride=2, padding=1),
+            #     nn.Conv2d(input_channel, first_channel, kernel_size=3, stride=2, padding=1),  # output: (N, 512, 12, 39)
             #     nn.BatchNorm2d(first_channel),
             #     nn.ReLU(),
-            #     nn.AdaptiveAvgPool2d((1, 1)),
+            #     nn.Conv2d(first_channel, 1024, kernel_size=3, stride=2, padding=1),  # output: (N, 512, 6, 20)
+            #     nn.BatchNorm2d(1024),
+            #     nn.ReLU(),
+            #     nn.Conv2d(1024, 2048, kernel_size=3, stride=2, padding=1),  # output: (N, 1024, 3, 10)
+            #     nn.BatchNorm2d(2048),
+            #     nn.ReLU(),
+            #     nn.AdaptiveAvgPool2d((1, 1)), # output: (N, 1024, 1, 1)
             #     nn.Flatten(),
-            #     nn.Linear(first_channel, 256),
+            #     nn.Linear(2048, 512),
+            #     nn.BatchNorm1d(512),
+            #     nn.ReLU(),
+            #     nn.Linear(512, 256),
+            #     nn.BatchNorm1d(256),
+            #     nn.ReLU(),
+            #     nn.Linear(256, 256),
             #     nn.BatchNorm1d(256),
             #     nn.ReLU(),
             #     nn.Linear(256, 1),
             #     nn.Sigmoid()
             #     )
+            self.classifier_layers = nn.Sequential(
+                nn.Conv2d(input_channel, first_channel, kernel_size=3, stride=2, padding=1),
+                nn.BatchNorm2d(first_channel),
+                nn.ReLU(),
+                nn.AdaptiveAvgPool2d((1, 1)),
+                nn.Flatten(),
+                nn.Linear(first_channel, 256),
+                nn.BatchNorm1d(256),
+                nn.ReLU(),
+                nn.Linear(256, 1),
+                nn.Sigmoid()
+                )
         elif model_name == 'resnet18_all':
             self.classifier_layers = nn.Sequential(
                 # nn.Conv2d(input_channel, first_channel, kernel_size=3, stride=2, padding=1),  # output: (N,
