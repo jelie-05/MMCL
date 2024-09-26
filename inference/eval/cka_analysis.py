84753be6194fcb2b401ef41387c687c9b6fa8cc3
jelie-05 <jeremialieks@gmail.com>
2024-09-26 18:28:36 +0200

    eval aug all

--- inference/eval/cka_analysis.py
+++ inference/eval/cka_analysis.py
@@ -60,7 +60,7 @@ def cka_analysis(data_root, output_dir, model_1, model_2, tag_1, tag_2, title, p
 
     batch_size = 64
     num_cores = min(multiprocessing.cpu_count(), 64)
-    dataloader_im, dataloader_lid, dataloader_neg = create_dataloaders(root=data_root, perturb_filenames=perturbation_eval, mode='eval',
+    dataloader_im, dataloader_lid, dataloader_neg = create_dataloaders(root=data_root, perturb_filenames=perturbation_eval, mode='val',
                                                                        batch_size=batch_size, num_cores=num_cores)
 
     if 'Image' in tag_1:
