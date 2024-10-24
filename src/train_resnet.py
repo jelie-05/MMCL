075f626e3b57e1cb0296553fb54c2494b9c186c7
jelie-05 <jeremialieks@gmail.com>
2024-10-24 19:42:18 +0200

    train 25

--- src/train_resnet.py
+++ src/train_resnet.py
@@ -48,10 +48,10 @@ def main(args, project_root, save_name, vit, masking, logger_launch='True', trai
     # Get the number of available CPU cores
     num_cores = min(multiprocessing.cpu_count(), 64)
     data_root = os.path.join(project_root, dataset_path)
-    # train_gen = DataGenerator(data_root, 'train', perturb_filenames=perturbation_file, augmentation=augmentation)
-    # val_gen = DataGenerator(data_root, 'val', perturb_filenames=perturbation_file, augmentation=augmentation)
-    train_gen = DataGenerator(data_root, 'debug', perturb_filenames=perturbation_file, augmentation=augmentation)
-    val_gen = DataGenerator(data_root, 'debug', perturb_filenames=perturbation_file, augmentation=augmentation)
+    train_gen = DataGenerator(data_root, 'train', perturb_filenames=perturbation_file, augmentation=augmentation)
+    val_gen = DataGenerator(data_root, 'val', perturb_filenames=perturbation_file, augmentation=augmentation)
+    # train_gen = DataGenerator(data_root, 'debug', perturb_filenames=perturbation_file, augmentation=augmentation)
+    # val_gen = DataGenerator(data_root, 'debug', perturb_filenames=perturbation_file, augmentation=augmentation)
     train_loader = train_gen.create_data(batch_size=batch_size, shuffle=True, nthreads=num_cores)
     val_loader = val_gen.create_data(batch_size=batch_size, shuffle=False, nthreads=num_cores)
     # --
